c74918799d92b290681da6169f98d21d58bf1e0c
AmosArdnach-6152 <102367472+AmosArdnach-6152@users.noreply.github.com>
2022-12-19 10:26:01 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,15 @@
+const { response } = require("express")
+const express = require("express")
+const app = express()
+
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.post("/upload", (req, res) => {
+    res.send("hi")
+})
+
+app.listen(3000)
